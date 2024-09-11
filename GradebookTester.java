7117f236c08d48fe2f4d84dcc77014e648777ecd
bobelai <158115164+bobelai@users.noreply.github.com>
2024-09-10 23:43:13 -0400

    Add files via upload

---
+++ GradebookTester.java
@@ -0,0 +1,55 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GradebookTester {
+
+    private GradeBook g1;
+    private GradeBook g2;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        g1 = new GradeBook(5);
+        g2 = new GradeBook(5);
+
+        g1.addScore(50);
+        g1.addScore(75);
+
+        g2.addScore(85);
+        g2.addScore(95);
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        g1 = null;
+        g2 = null;
+    }
+
+    @Test
+    public void testAddScore() {
+        assertTrue(g1.toString().equals("50.0 75.0 "));
+        assertTrue(g2.toString().equals("85.0 95.0 "));
+        assertEquals(2, g1.getScoreSize());
+        assertEquals(2, g2.getScoreSize());
+    }
+
+    @Test
+    public void testSum() {
+        assertEquals(125.0, g1.sum(), 0.0001);
+        assertEquals(180.0, g2.sum(), 0.0001);
+    }
+
+    @Test
+    public void testMinimum() {
+        assertEquals(50.0, g1.minimum(), 0.001);
+        assertEquals(85.0, g2.minimum(), 0.001);
+    }
+
+    @Test
+    public void testFinalScore() {
+        assertEquals(75.0, g1.finalScore(), 0.0001);  // 125 - 50 = 75
+        assertEquals(95.0, g2.finalScore(), 0.0001);  // 180 - 85 = 95
+    }
+}
